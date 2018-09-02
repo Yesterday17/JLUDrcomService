4ae3c083e1a73074144e8962f9a84a2b0bbe30f2
Yesterday17 <t@yesterday17.cn>
2018-09-02 10:28:47 +0800

    Refactor.

--- JLUDrcomService/JLUDrcomService.cs
+++ JLUDrcomService/JLUDrcomService.cs
@@ -15,10 +15,7 @@ namespace JLUDrcomService
     {
         private RegistryKey jlu;
 
-        private String username;
-        private String password;
-
-        private byte[] tail = new byte[2];
+        private Network client;
         public JLUDrcomService()
         {
             // 初始化
@@ -26,9 +23,8 @@ namespace JLUDrcomService
             Registry.CurrentUser.CreateSubKey(@"SOFTWARE\JLUDrcomService");
             jlu = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\JLUDrcomService");
 
-            // 设置用户名密码
-            username = ReadRegistry("username");
-            password = ReadRegistry("password");
+            // 配置 client
+            client = new Network(ReadRegistry("username"), ReadRegistry("password"));
         }
 
         protected override void OnStart(string[] args)
@@ -36,15 +32,15 @@ namespace JLUDrcomService
             // 调试用 手动创造断点
             System.Diagnostics.Debugger.Launch();
 
-            // 发送 Challenge 包
+            // 发送 StartRequest 包
             int retry = 0;
-            while (!Network.Challenge(retry))
+            while (!client.StartRequest(retry))
             {
                 retry++;
             }
 
             // 使用用户名密码登录
-            if (!Network.Login(username, password))
+            if (!client.LoginAuth())
             {
                 this.Stop();
             }
@@ -73,7 +69,7 @@ namespace JLUDrcomService
 
         private void HeartBeat_Tick(object sender, EventArgs e)
         {
-            Network.HeartBeat(tail);
+            client.HeartBeat();
         }
 
         private String ReadRegistry(String key)
--- JLUDrcomService/Network.cs
+++ JLUDrcomService/Network.cs
@@ -13,11 +13,38 @@ namespace JLUDrcomService
         private const String authServer = "10.100.61.3";
         private const int authPort = 61440;
 
-        public static bool Challenge(int retry)
+
+        private String username;
+        private String password;
+        private byte[] tail = new byte[2];
+
+        enum Code
+        {
+            StartRequest = 0x01,
+            StartResponse = 0x02,
+            LoginAuth = 0x03,
+            Success = 0x04,
+            Failure = 0x05,
+            LogoutAuth = 0x06,
+            Misc = 0x07,
+            Unknown = 0x08,
+            NewPassword = 0x09,
+            Message = 0x4d,
+            Alive4Client = 0xfe,
+            Alive = 0xff
+        }
+
+        public Network(String username, String password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public bool StartRequest(int retry)
         {
             byte[] packet = new byte[]
             {
-                0x01, (byte)(0x02 + retry),
+                (byte)Code.StartRequest, (byte)(0x02 + retry),
                 NetworkUtils.RandomByte(), NetworkUtils.RandomByte(),
                 0x6a, 0x00,
                 0x00, 0x00,
@@ -29,17 +56,22 @@ namespace JLUDrcomService
                 0x00, 0x00
             };
             byte[] receive = NetworkUtils.SendUDPDatagram(packet);
-            return true;
+            return receive[0] == 0x02;
         }
 
-        public static bool Login(String username, String password)
+        public bool LoginAuth()
         {
             return true;
         }
 
-        public static String HeartBeat(byte[] tail)
+        public bool LogoutAuth()
         {
-            return "";
+            return true;
+        }
+
+        public bool HeartBeat()
+        {
+            return true;
         }
 
         static class NetworkUtils
